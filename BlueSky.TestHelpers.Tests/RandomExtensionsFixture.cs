0b20f1b983781fb04ae4ab47d0b7727ef7e8acea
kiran-singh <kiran_cardiff@yahoo.co.uk>
2014-06-05 22:36:50 +0200

    Files missed in last commit

---
+++ BlueSky.TestHelpers.Tests/RandomExtensionsFixture.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BlueSky.TestHelpers.Tests
+{
+    [TestFixture]
+    public class RandomExtensionsFixture
+    {
+        public class Sample
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        private Random _random;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _random = new Random();
+        }
+
+        [Test]
+        public void ListFor_MinAndMaxProvided_ReturnsListBetweenMinAndMax()
+        {
+            // Arrange
+            var next = _random.Next(50, 100);
+            var min = RandomExtensions.MinForList + next;
+            var max = RandomExtensions.MaxForList + next;
+
+            // Act
+            var actual = _random.ListFor<Sample>(min, max);
+
+            // Assert
+            actual.Count.Should().BeGreaterThan(min);
+            actual.Count.Should().BeLessThan(max);
+        }
+
+        [Test]
+        public void ListFor_NoMinAndMaxProvided_ReturnsListBetweenDefaultMinAndMax()
+        {
+            // Arrange
+            // Act
+            var actual = _random.ListFor<Sample>();
+
+            // Assert
+            Assert.IsTrue(actual.Count > RandomExtensions.MinForList);
+            Assert.IsTrue(actual.Count < RandomExtensions.MaxForList);
+        }
+
+        [Test]
+        public void FilledListFor_ActionProvided_MinAndMaxProvided_ReturnsListBetweenMinAndMax()
+        {
+            // Arrange
+            // Act
+            var actual = _random.FilledListFor<Sample>(x =>
+            {
+                x.Id = _random.Next();
+                x.Name = _random.Next().ToString();
+            });
+
+            // Assert
+            Assert.IsTrue(actual.Count > RandomExtensions.MinForList);
+            Assert.IsTrue(actual.Count < RandomExtensions.MaxForList);
+            actual.ToList().ForEach(x =>
+            {
+                x.Id.Should().BeGreaterThan(0);
+                x.Name.Should().NotBeNullOrEmpty();
+            });
+        }
+
+        [Test]
+        public void FilledListFor_ActionProvided_NoMinAndMaxProvided_ReturnsListBetweenDefaultMinAndMax()
+        {
+            // Arrange
+            var next = _random.Next(50, 100);
+            var min = RandomExtensions.MinForList + next;
+            var max = RandomExtensions.MaxForList + next;
+
+            // Act
+            var actual = _random.FilledListFor<Sample>(x =>
+            {
+                x.Id = _random.Next();
+                x.Name = _random.Next().ToString();
+            }, min, max);
+
+            // Assert
+            Assert.IsTrue(actual.Count > min);
+            Assert.IsTrue(actual.Count < max);
+            actual.ToList().ForEach(x =>
+            {
+                x.Id.Should().BeGreaterThan(0);
+                x.Name.Should().NotBeNullOrEmpty();
+            });
+        }
+    }
+}
---
+++ BlueSky.TestHelpers/RandomExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BlueSky.Common;
+
+namespace BlueSky.TestHelpers
+{
+    public static class RandomExtensions
+    {
+        public const int MinForList = 10;
+        public const int MaxForList = 20;
+
+        public static IList<T> ListFor<T>(this Random random, int min = MinForList, int max = MaxForList)
+            where T : new()
+        {
+            var list = new List<T>();
+            Enumerable.Range(1, random.Next(min, max)).ForEach(x => list.Add(new T()));
+            return list;
+        }
+
+        public static IList<T> FilledListFor<T>(this Random random, Action<T> action, int min = MinForList,
+            int max = MaxForList)
+            where T : new()
+        {
+            var list = random.ListFor<T>(min, max);
+            list.ForEach(action);
+            return list;
+        }
+    }
+
+
+}
